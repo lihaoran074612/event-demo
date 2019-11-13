7f35b5d5b5ba1f5a6eeafd8100565c31e652b9db
李浩燃 <577979102@qq.com>
2019-11-13 14:50:51 +0800

    version

--- src/main/java/com/srm/event/demo/controller/EventController.java
+++ src/main/java/com/srm/event/demo/controller/EventController.java
@@ -1,19 +1,23 @@
 package com.srm.event.demo.controller;
 
+import com.srm.event.demo.entity.User;
 import com.srm.starter.EventPublishUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class EventController {
     @Autowired
     EventPublishUtil eventPublishUtil;
 
-    @GetMapping("/event/{obj}/{code}")
-    public String eventTest(@PathVariable String obj, @PathVariable String code){
-        eventPublishUtil.publishEvent(obj,code);
+    @GetMapping("/event/{code}")
+    public String eventTest(@PathVariable String code){
+        User user = new User(18,"小小二","武汉大学",new Date());
+        eventPublishUtil.publishEvent(user,code);
         return code;
     }
 }
